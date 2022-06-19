4b5a94d43c49adf7f9371337d49092fd8d4746be
Santiago Sommer <santisommer52@gmail.com>
2022-06-19 18:05:35 -0300

    coso

--- cpp/ControladorHostales.cpp
+++ cpp/ControladorHostales.cpp
@@ -90,7 +90,7 @@ bool ControladorHostales::existeHostal(string nombreHostal)
 }
 
 
-DTHostal ControladorHostales::getDTHostal(){
+DTHostal ControladorHostales::getDTHostal(string){
     return DTHostal("nombre","nombre",1,1); //TEMPORAL.
 }
 set<string> ControladorHostales::getHostales(){
